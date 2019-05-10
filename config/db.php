463539a0fbf428946c82af96eb7c662e3463919c
Gustavo Biott <gustavo.biott@mobisoft.com.ar>
2019-05-10 15:52:36 -0300

    init

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=accounting',
+    'dsn' => 'pgsql:host=apps.biott.com.ar;dbname=accounting',
     'username' => 'biotticos',
     'password' => 'sogtulakk',
     'charset' => 'utf8',
