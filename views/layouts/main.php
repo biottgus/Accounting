518571a9ab27205a9e2cfe9f8fc0add49bc38b8d
Gustavo Biott <gustavo.biott@mobisoft.com.ar>
2019-05-16 14:52:06 -0300

    modificaciones vairas

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -37,7 +37,7 @@ AppAsset::register($this);
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
+                    ['label' => 'Home', 'url' => ['/index.php?r=account-transactions%2Fcreate']],
                     ['label' => 'Movimientos',
                         'url' => '#',
                         'items' => [
