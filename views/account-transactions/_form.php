374d62e3b1635485a746f39a938dcf4aeb668195
Gustavo Biott <gustavo.biott@mobisoft.com.ar>
2019-06-04 12:45:22 -0300

    refresh

--- views/account-transactions/_form.php
+++ views/account-transactions/_form.php
@@ -11,18 +11,19 @@ use yii\widgets\ActiveForm;
 
 <div class="account-transactions-form">
 
-    <?php 
-    $form = ActiveForm::begin(); 
-    $model->date_account_transactions = date('Y-m-d');
-    
+    <?php
+    $form = ActiveForm::begin();
+    if (!$model->date_account_transactions)
+        $model->date_account_transactions = date('Y-m-d');
+
 //    echo $form->field($model, 'datetime_account_transactions')->textInput() ;
     echo $form->field($model, 'id_accounts')->dropDownList(ArrayHelper::map(app\models\Accounts::find()->orderBy('name_accounts')->all(), 'id_accounts', 'name_accounts'));
-    echo $form->field($model, 'date_account_transactions')->input('date') ;
+    echo $form->field($model, 'date_account_transactions')->input('date');
     echo $form->field($model, 'id_currency')->dropDownList(ArrayHelper::map(app\models\Currencies::find()->orderBy('iso_currency')->all(), 'id_currency', 'iso_currency'));
-    echo $form->field($model, 'value_account_transactions')->input('decimal') ;
-    echo $form->field($model, 'concept_account_transactions')->textarea() ;
-    echo $form->field($model, 'document_account_transactions')->textInput() ;
-    echo $form->field($model, 'id_users')->hiddenInput(['value'=> '1'])->label(false);
+    echo $form->field($model, 'value_account_transactions')->input('decimal');
+    echo $form->field($model, 'concept_account_transactions')->textarea();
+    echo $form->field($model, 'document_account_transactions')->textInput();
+    echo $form->field($model, 'id_users')->hiddenInput(['value' => '1'])->label(false);
     ?>
 
     <div class="form-group">
