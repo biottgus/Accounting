efeb92c66fd27529545a4e3823c28a1e79c6fdce
Gustavo Biott <gustavo.biott@mobisoft.com.ar>
2019-10-15 16:23:46 -0300

    categorias

--- views/report-categories/results.php
+++ views/report-categories/results.php
@@ -5,9 +5,8 @@
  * 
  */
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
 use yii\data\ArrayDataProvider;
-use yii\web\JsExpression;
 
 /**
  * 
@@ -21,7 +20,7 @@ function queryBase($params) {
     $sSQL = "SELECT a.id_account_transactions, a.date_account_transactions, a.id_accounts, 
 	a.value_account_transactions, a.id_currency, a.concept_account_transactions, a.document_account_transactions,
 	b.id_categories, b.name_categories, b.name_accounts, b.id_account_types, b.name_account_types, b.type_account_types,
-	c.name_currency
+	c.iso_currency
         FROM account_transactions AS a
         LEFT JOIN view_account_categories AS b ON (b.id_accounts=a.id_accounts)
         LEFT JOIN currencies AS c ON (c.id_currency=a.id_currency)
@@ -49,25 +48,73 @@ function reporteAnalisis($model, $data) {
 
     $toPrint = GridView::widget([
                 'dataProvider' => $dataProvider,
-                'showFooter' => true,
+                // desde aca es krakik
+                'pjax' => true,
+                'striped' => true,
+                'hover' => true,
+                'panel' => [
+                    'type' => 'success',
+                    'footer' => true,
+                    'heading' => 'Transacciones'
+                ],
+                'floatHeader' => false,
+                'condensed' => true,
+                'responsive' => true,
+                'responsiveWrap' => false,
+                // desde aca es krakik
                 'columns' => [
                     [
-                        'label' => 'Agrupado',
-                        'attribute' => 'agrupado',
+                        'label' => 'Categoría',
+                        'attribute' => 'name_categories',
+                        'pageSummary' => 'Total',
+//                        'pageSummaryOptions' => ['colspan' => 1],
+                        'group' => true,
+                        'groupedRow' => true,
+                    ],
+                    [
+                        'label' => 'Tipo',
+                        'attribute' => 'name_account_types',
+                        'group' => true,
+                    ],
+                    [
+                        'label' => 'Cuenta',
+                        'attribute' => 'name_accounts',
+                        'group' => true,
+                    ],
+                    [
+                        'label' => 'ID',
+                        'attribute' => 'id_account_transactions',
                         'footer' => 'Totales',
                         'headerOptions' => ['style' => 'text-align:center'],
                         'contentOptions' => ['style' => 'text-align:left'],
                     ],
                     [
-                        'label' => 'Total',
-                        'attribute' => 'total',
+                        'label' => 'Detalle',
+                        'attribute' => 'concept_account_transactions',
+                    ],
+                    [
+                        'label' => 'Moneda',
+                        'attribute' => 'iso_currency',
+                    ],
+                    [
+                        'label' => 'Importe',
+                        'attribute' => 'value_account_transactions',
                         'format' => ['decimal', '2'],
-                        'footer' => $model->getTotalColumn($dataProvider->models, 'total'),
+//                        'footer' => $model->getTotalColumn($dataProvider->models, 'total'),
                         'headerOptions' => ['style' => 'text-align:center'],
                         'contentOptions' => ['style' => 'text-align:right'],
                         'footerOptions' => ['style' => 'text-align:right; font-weight: bold;'],
+                        'value' => function($dataProvider) {
+                            $value = $dataProvider['value_account_transactions'] * $dataProvider['type_account_types'];
+                            return $value;
+                            ;
+                        },
+                        'hAlign' => 'right',
+                        'vAlign' => 'middle',
+                        'pageSummary' => true,
                     ],
                 ],
+                'showPageSummary' => true,
     ]);
 
     return $toPrint;
@@ -86,6 +133,4 @@ echo "<h1>Reporte por Categoría</h1>";
 $sqlBase = queryBase($reportParams);
 
 $data = Yii::$app->db->createCommand($sqlBase)->queryAll();
-var_dump( $data);
-
-//echo reporteAnalisis($model, $data);
+echo reporteAnalisis($model, $data);
